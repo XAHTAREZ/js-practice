ad1ba1aa9ef8043fa25a0bc72977d1c1dbece38a
Denisemail <barovdenis@mail.ru>
2022-02-16 14:05:35 +0300

    practice-2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 // ПРАКТИКА 1
 
+/*
+
 // Первое задание
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
@@ -25,3 +27,42 @@ let a = prompt("Последний просмотренный фильм?", ""),
 
 personalMovieDB.movies.a = b;
 personalMovieDB.movies.c = d;
+
+*/
+
+// ПРАКТИКА 2
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+// Первое задание
+// Второе задание
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Последний просмотренный фильм?", "");
+  let b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 10) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+//Третье задание
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Ошибка!");
+}
